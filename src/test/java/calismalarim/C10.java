8600c8f0dce988a3953882a70fa7965085fe2b34
Ali Osman <aliosmankorkmaz61@hotmail.com>
2023-06-12 16:17:38 +0300

    12.06.23

---
+++ src/test/java/calismalarim/C10.java
@@ -0,0 +1,40 @@
+package Group_Studies.DropDown;
+
+import UtilClass.TestBase;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class C10 extends TestBase {
+    /*
+    -Amazon sayfasına gidelim
+    -Arama Kutusundaki Dropdown menuyu yazdıralım
+    -Dropdown menuden sırasıyla ilk 5 başlığı (Arts&Crafts ile başlayıp Books'a kadar Books dahil) seçip
+    başlık altındakileri aratalım. Her aramada sayfa başlığını yazdıralım
+    Not: Select Class'ı kullanalım
+     */
+
+    @Test
+    public void tes01() {
+        //-Amazon sayfasına gidelim
+        driver.get("https://amazon.com");
+        //-Arama Kutusundaki Dropdown menuyu yazdıralım
+        WebElement categories=driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+        Select select=new Select(categories);
+        select.getOptions().forEach(w-> System.out.println(w.getText()));
+        //-Dropdown menuden sırasıyla ilk 5 başlığı (Arts&Crafts ile başlayıp Books'a kadar Books dahil) seçip
+        //    başlık altındakileri aratalım. Her aramada sayfa başlığını yazdıralım
+
+        for (int i = 1; i <6 ; i++) {
+            categories=driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+            select=new Select(categories);
+            select.selectByIndex(i);
+            wait(2);
+            driver.findElement(By.xpath("//*[@id='nav-search-submit-button']")).click();
+            System.out.println(i+". Title = " + driver.getTitle());
+            wait(1);
+        }
+    }
+}
---
+++ src/test/java/calismalarim/TestBase1.java
@@ -0,0 +1,39 @@
+package calismalarim;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
+public abstract class TestBase1 {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    public void wait(int i) {
+        try {
+            Thread.sleep(i * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void back() {
+        driver.navigate().back();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
