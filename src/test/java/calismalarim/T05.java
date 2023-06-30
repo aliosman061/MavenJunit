436f32b5d38291573f84c9d4444cab08832c8f3a
Ali Osman <aliosmankorkmaz61@hotmail.com>
2023-06-30 22:43:18 +0300

    30.06.23

---
+++ src/test/java/calismalarim/T05.java
@@ -0,0 +1,41 @@
+package calismalarim;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class T05 extends TestBase {
+    // https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/ sitesine gidin
+// ikinci emojiye tıklayın
+// İkinci emoji öğelerini yazdırınız
+// Parent iframe e geri donun
+// Formu doldurun,(Formu istediğiniz metinlerle doldurun)
+// Apply button a basiniz
+    @Test
+    public void test02() throws InterruptedException {
+        driver.get("https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/");
+        WebElement iframe = driver.findElement(By.xpath("//*[@id='emoojis']"));
+        driver.switchTo().frame(iframe);
+        driver.findElement(By.xpath("(//*[@data-upgraded=',MaterialRipple'])[2]")).click();
+        List<WebElement> emojiler = driver.findElements(By.xpath("//*[@id='nature']//div"));
+        for (WebElement w:emojiler) {
+            System.out.println(w.getText());
+        }
+        driver.switchTo().defaultContent();
+
+        List<WebElement> list = driver.findElements(By.xpath("//input[@class='mdl-textfield__input']"));
+
+        List<String> veriler = new ArrayList<>(Arrays.asList(
+                "Erol","Evren","selenium","lambda","java","sql","gitgithub","fsd","dfasf","lkjl","asdasd","asd","sda"));
+
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).sendKeys(veriler.get(i));
+        }
+        driver.findElement(By.xpath("//*[text()='Apply']")).click();
+    }
+}
