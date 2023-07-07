20331177973c80de7c1ad45b119732e6445bb271
Ali Osman <aliosmankorkmaz61@hotmail.com>
2023-07-08 01:25:38 +0300

    08.07.23

---
+++ src/test/java/techproed/day23_Log4j/C02_Log4j.java
@@ -0,0 +1,27 @@
+package techproed.day23_Log4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+
+
+
+public class C02_Log4j extends TestBase {
+    @Test
+    public void test01() {
+        Logger logger = LogManager.getLogger(C02_Log4j.class);
+
+        //Techproeducation sayfasina gidelim
+        driver.get("https://techproeducation.com");
+        logger.info("Techpro Sayfasina Gidildi");
+        driver.findElement(By.xpath("//i[@class='eicon-close']")).click();
+        logger.info("Sayfada Cikan Reklam Kapatildi");
+        //Basligin egitim icerdigini dogrulayalim
+        Assert.assertFalse(driver.getTitle().contains("Egitim"));
+        logger.error("Sayfa Basligi Egitim Yazisi Icermiyor");
+    }
+}
